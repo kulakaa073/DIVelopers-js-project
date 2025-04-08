fad93ca95086aa883b48d80b96b0665f59f87a30
Shade <kulakaa073@gmail.com>
2025-04-09 01:19:03 +0300

    more fixes and cleanups

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,11 +1,9 @@
-import Accordion from "accordion-js";
-
+import Accordion from 'accordion-js';
 
 const according = document.querySelector('.faq-acc-container');
 const listItemAc = document.querySelectorAll('.ac');
 
 new Accordion(according, {
-  openOnInit: [0],
   collapse: true,
   duration: 400,
   showMultiple: false,
